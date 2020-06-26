43a5543f0fab136e56aae0d5603efca7ae3d6785
Hunter Gregory <hlg16@duke.edu>
2020-06-26 17:32:23 -0400

    uses prices now (just built them before)

--- data_generator.py
+++ data_generator.py
@@ -57,7 +57,7 @@ class TimeSeriesSequence(Sequence):
                 prices[k, week_beginning:week_beginning + 7] = sell_prices.iloc[j].sell_price
             prices[k,-2] = sell_prices.iloc[len(sell_prices)-2].sell_price
             prices[k,-1] = sell_prices.iloc[len(sell_prices)-1].sell_price # all weeks have 7 days except last (2 days)
-            prices = np.reshape(prices, (actual_size, MAX_SERIES_LENGTH + HORIZON_LENGTH))
+        prices = np.reshape(prices, prices.shape + (1,))
 
         for k in range(actual_size):
             first_sale_day = first_sale_days[k+idx*self.batch_size]
@@ -65,11 +65,18 @@ class TimeSeriesSequence(Sequence):
             tiled_day_labels[k, 0:first_sale_day, :] = 0
             prices[k, 0:first_sale_day] = 0
 
+        features_concats = [
+            prices[:, :(MAX_SERIES_LENGTH + HORIZON_LENGTH)],
+            tiled_day_labels[:, :(MAX_SERIES_LENGTH + HORIZON_LENGTH)], 
+            tiled_static_features
+        ]
+        features = np.concatenate(features_concats, axis=-1)
+
         time_series_batch = self.time_series[start:end]
         x_batch = np.reshape(time_series_batch, time_series_batch.shape + (1,))
-        x_batch = np.concatenate([x_batch, tiled_day_labels[:, :MAX_SERIES_LENGTH], tiled_static_features[:, :MAX_SERIES_LENGTH]], axis=-1)
+        x_batch_concats = [x_batch, features[:, :MAX_SERIES_LENGTH]]
+        x_batch = np.concatenate(x_batch_concats, axis=-1)
 
-        features = np.concatenate([tiled_day_labels[:, :(MAX_SERIES_LENGTH + HORIZON_LENGTH)], tiled_static_features], axis=-1)
         pred_features = []
         for i in range(features.shape[0]):
             series = features[i]
@@ -98,95 +105,3 @@ class TimeSeriesSequence(Sequence):
         if self.weights is not None:
             return full_batch + (self.weights[start:end],)
         return full_batch
-
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-
-from time import time
-
-import tensorflow as tf
-import tensorflow.keras.backend as K
-from tensorflow.keras.layers import Concatenate, Dense, Dropout, Input, Lambda, LSTM
-from tensorflow.keras.models import Model
-from tensorflow.keras.initializers import GlorotUniform, Orthogonal
-from tensorflow.keras.callbacks import EarlyStopping
-
-from constants import DTYPE, HORIZON_LENGTH, QUANTILES, MAX_SERIES_LENGTH, LOSS_SIZE, SALES_DATA_PATH, CALENDAR_DATA_PATH
-
-from mlp import MLP
-from pinball import pinball, PinballMetric
-from data_generator import TimeSeriesSequence
-
-"""
-TODO PINBALL LOSS
-- look at histogram of sales for all item-level stuff
-    - if similar, leave pinball as is
-    - eventually add scaling
-    - eventually add masking for last H observations
-TODO
-- normalize each sample (individually)?
--  hierarchy
-- speed
-"""
-
-## CONSTANTS
-TESTING = True
-
-TEMPORAL_CONTEXT_SIZE = 2 * len(QUANTILES)
-TIME_AGNOSTIC_CONTEXT_SIZE = 10
-RNN_UNITS = 128
-
-EPOCHS = 2 if TESTING else 20
-BATCH_SIZE = 32
-VAL_SPLIT = 0.2
-FIT_VERBOSITY = 2
-PREDICT_VERBOSITY = 1 if TESTING else 0
-NUM_OBSERVATIONS = 64 # for testing
-
-## LOAD DATA
-sales_df = pd.read_csv(SALES_DATA_PATH)
-if TESTING: 
-    sales_df = sales_df.iloc[0:NUM_OBSERVATIONS]
-
-features = sales_df.loc[:,['dept_id', 'store_id']]
-encoder = OneHotEncoder(categories='auto', drop='first', sparse=False)
-one_hot_features = encoder.fit_transform(features)
-
-train_sales_columns = sales_df.columns[6:]
-item_store_ids = [(row.item_id, row.store_id) for _, row in sales_df.iterrows()]
-x_train = sales_df.loc[:, train_sales_columns].to_numpy()
-
-# DATA DEPENDENT CONSTANTS
-assert(MAX_SERIES_LENGTH == len(sales_df.columns) - 6)
-NUM_STATIC_FEATURES = one_hot_features.shape[1]
-NUM_DAY_LABELS = 6
-calendar_map = {'National': 2, 'Religious': 3, 'Cultural': 4, 'Sporting': 5} # NaN is 0, Xmas is 1
-TIME_OBSERVATION_DIMENSION = NUM_STATIC_FEATURES + NUM_DAY_LABELS + 1 # eventually add price
-# MQ-RNN has static features in each time step
-
-calendar = pd.read_csv(CALENDAR_DATA_PATH)
-binary_day_labels = np.zeros((len(calendar), NUM_DAY_LABELS))
-for k in range(len(calendar)):
-    day = calendar.iloc[k]
-    if day.event_name_1 is np.nan:
-        binary_day_labels[k][0] = 1
-    else:
-        if day.event_name_1 == 'Christmas':
-            binary_day_labels[k][1] = 1
-        else:
-            binary_day_labels[k][calendar_map[day.event_type_1]] = 1
-        if day.event_name_2 == 'Christmas':
-            binary_day_labels[k][1] = 1
-        elif day.event_name_2 is not np.nan:
-            binary_day_labels[k][calendar_map[day.event_type_2]] = 1
-
-data_sequence = TimeSeriesSequence(x_train, item_store_ids, one_hot_features, binary_day_labels, BATCH_SIZE)
-
-from time import time
-before = time()
-data_sequence.__getitem__(0)
-
-data_sequence.__getitem__(data_sequence.__len__() - 1)
-after = time()
-print(after - before)
