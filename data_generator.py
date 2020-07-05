4d06c7b0485bcd63599db4c5ad6c34eab6b948de
Hunter Gregory <hlg16@duke.edu>
2020-07-05 18:46:22 -0400

    shuffling handled in sequence constructor now. number of observations detected instead of hard coded

--- data_generator.py
+++ data_generator.py
@@ -19,10 +19,6 @@ sell_price_df = pd.read_csv(SELL_PRICES_DATA_PATH)
 
 sample_weights = np.array(pd.read_csv(WEIGHTS_PATH).spl_scaled_weight)
 
-# seed(50)
-# series_indices = [k for k in range(64)] # FIXME 42840 for testing
-# shuffle(series_indices)
-
 ## CURRENT RUNTIME: 18.2 / 2
 ## 2nd optimization: 15.1 / 2 (or 16.6, 15.4)
 ## 3rd optimization: 8.8 / 2 (or 9.5)
@@ -32,8 +28,14 @@ class TimeSeriesSequence(Sequence):
     # item_store_ids: (n) [tuple of (item_id, store_id)]
     # static_features: (n, num_static_labels). One-hot encoded
     # day_labels: (MAX_SERIES_LENGTH + x, num_day_labels). Binary
-    # weights: (n,). Leave as None for no sample weighting
-    def __init__(self, time_series, static_features, day_labels, batch_size, use_weights=True):
+    # shuffle: boolean whether to shuffle data or not
+    # use_weights: boolean whether to include sample weights in output or not
+    def __init__(self, time_series, static_features, day_labels, batch_size, should_shuffle=True, use_weights=True):
+        self.series_indices = [k for k in range(time_series.shape[0])]
+        if should_shuffle:
+            seed(50)
+            shuffle(self.series_indices)
+
         self.time_series = time_series
         self.static_features = np.reshape(static_features, (static_features.shape[0], 1, static_features.shape[1]))
         self.batch_size = batch_size
@@ -50,10 +52,10 @@ class TimeSeriesSequence(Sequence):
     def __getitem__(self, idx): # TODO back to __ at start
         start = idx * self.batch_size
         end = (idx + 1) * self.batch_size
-        # batch_indices = series_indices[start:end]
+        batch_indices = self.series_indices[start:end]
         actual_size = self.batch_size if end < len(self.time_series) else len(self.time_series) - idx * self.batch_size
 
-        tiled_static_features = np.tile(self.static_features[start:end], (1, MAX_SERIES_LENGTH + HORIZON_LENGTH, 1))
+        tiled_static_features = np.tile(self.static_features[batch_indices], (1, MAX_SERIES_LENGTH + HORIZON_LENGTH, 1))
         tiled_day_labels = self.tiled_day_labels[:actual_size]
 
         features_concats = [
@@ -62,7 +64,7 @@ class TimeSeriesSequence(Sequence):
         ]
         features = np.concatenate(features_concats, axis=-1)
 
-        time_series_batch = self.time_series[start:end, :MAX_SERIES_LENGTH]
+        time_series_batch = self.time_series[batch_indices, :MAX_SERIES_LENGTH]
         x_batch = np.reshape(time_series_batch, time_series_batch.shape + (1,))
         x_batch_concats = [x_batch, features[:, :MAX_SERIES_LENGTH]]
         x_batch = np.concatenate(x_batch_concats, axis=-1)
@@ -91,6 +93,5 @@ class TimeSeriesSequence(Sequence):
             {'train_forecasts': y_batch, 'quantile_forecasts': val_y_batch}
         )
         if self.use_weights:
-            return full_batch + (sample_weights[start:end],)
+            return full_batch + (sample_weights[batch_indices],)
         return full_batch
-
