964b8f406d28917e34decd62607f0f63a1b16a4d
Hunter Gregory <hlg16@duke.edu>
2020-07-05 18:31:50 -0400

    removed unnecessary comments, updated pinball metric sample_weights logic (sample weights are never used though)

--- data_generator.py
+++ data_generator.py
@@ -19,11 +19,11 @@ sell_price_df = pd.read_csv(SELL_PRICES_DATA_PATH)
 
 sample_weights = np.array(pd.read_csv(WEIGHTS_PATH).spl_scaled_weight)
 
-seed(50)
-series_indices = [k for k in range(64)] # FIXME 42840 for testing
-shuffle(series_indices)
+# seed(50)
+# series_indices = [k for k in range(64)] # FIXME 42840 for testing
+# shuffle(series_indices)
 
-## CURRENTLY 18.2 / 2
+## CURRENT RUNTIME: 18.2 / 2
 ## 2nd optimization: 15.1 / 2 (or 16.6, 15.4)
 ## 3rd optimization: 8.8 / 2 (or 9.5)
 
@@ -45,15 +45,15 @@ class TimeSeriesSequence(Sequence):
 
     # returns tuple of 2 or 3
     # x: [(batch_size, MAX_SERIES_LENGTH, 1 + num_static_labels + num_day_labels), (batch_size, MAX_SERIES_LENGTH, HORIZON_LENGTH, num_static_labels + num_day_labels)]
-    # y: {'train_forecasts': (    '     , MAX_SERIES_LENGTH - HORIZON_LENGTH, HORIZON_LENGTH, num_quantiles)}
+    # y: {'train_forecasts': (batch_size, MAX_SERIES_LENGTH - HORIZON_LENGTH, HORIZON_LENGTH, num_quantiles)}
     # w: (batch_size,) [optional]
     def __getitem__(self, idx): # TODO back to __ at start
         start = idx * self.batch_size
         end = (idx + 1) * self.batch_size
-        batch_indices = series_indices[start:end]
+        # batch_indices = series_indices[start:end]
         actual_size = self.batch_size if end < len(self.time_series) else len(self.time_series) - idx * self.batch_size
 
-        tiled_static_features = np.tile(self.static_features[batch_indices], (1, MAX_SERIES_LENGTH + HORIZON_LENGTH, 1))
+        tiled_static_features = np.tile(self.static_features[start:end], (1, MAX_SERIES_LENGTH + HORIZON_LENGTH, 1))
         tiled_day_labels = self.tiled_day_labels[:actual_size]
 
         # prices = np.zeros((actual_size, MAX_SERIES_LENGTH + HORIZON_LENGTH))
@@ -83,55 +83,35 @@ class TimeSeriesSequence(Sequence):
         ]
         features = np.concatenate(features_concats, axis=-1)
 
-        time_series_batch = self.time_series[batch_indices, :MAX_SERIES_LENGTH]
+        time_series_batch = self.time_series[start:end, :MAX_SERIES_LENGTH]
         x_batch = np.reshape(time_series_batch, time_series_batch.shape + (1,))
         x_batch_concats = [x_batch, features[:, :MAX_SERIES_LENGTH]]
         x_batch = np.concatenate(x_batch_concats, axis=-1)
 
-        # pred_features = []
-        # y_batch = []
         y_batch = np.zeros((actual_size, LOSS_SIZE, HORIZON_LENGTH, len(QUANTILES)))
-        # val_y_batch = np.zeros((actual_size, 1, HORIZON_LENGTH, len(QUANTILES)))
+        val_y_batch = np.zeros((actual_size, 1, HORIZON_LENGTH, len(QUANTILES)))
         pred_features = np.zeros((actual_size, MAX_SERIES_LENGTH, HORIZON_LENGTH, features.shape[-1]))
         for i in range(actual_size):
             feature_series = features[i]
-            # preds = []
-            # pred_features.append(preds)
-
-            # time_series = time_series_batch[i]
-            # expanded_time_series = []
-            # y_batch.append(expanded_time_series)
 
             for j in range(1, MAX_SERIES_LENGTH+1):
-                # preds_sublist = []
-                # preds.append(preds_sublist)
-                
                 should_expand = j-1 < LOSS_SIZE
-                # is_val_point = j == MAX_SERIES_LENGTH
-
-                # if should_expand:
-                #     expanded_sublist = []
-                #     expanded_time_series.append(expanded_sublist)
+                is_val_point = j == MAX_SERIES_LENGTH
 
                 for k in range(HORIZON_LENGTH):
                     pred_features[i,j-1,k,:] = feature_series[j+k]
-                    # preds_sublist.append(feature_series[j+k])
-                    if should_expand: # TODO if j-1 < MAX_SERIES_LENGTH, else...
+                    if should_expand:
                         y_batch[i,j-1,k,:] = self.time_series[i,j+k]
-                    # if is_val_point: # FIXME uncomment if can figure out metric sample weighting
-                    #     val_y_batch[i,0,k,:] = self.time_series[i,j+k]
-
-                        # expanded_sublist.append(len(QUANTILES) * [time_series[j+k]])
-        # pred_features = np.array(pred_features)
-        # y_batch = np.array(y_batch)
-        # now: 4, 4.1, 4.4, 4.5
+                    if is_val_point:
+                        val_y_batch[i,0,k,:] = self.time_series[i,j+k]
+        # runtime now: 4, 4.1, 4.4, 4.5
         # with lists: 7.7, 7.1
 
         full_batch = (
             {'time_series': x_batch, 'predict_features': pred_features},
-            {'train_forecasts': y_batch}
+            {'train_forecasts': y_batch, 'quantile_forecasts': val_y_batch}
         )
         if self.use_weights:
-            return full_batch + (sample_weights[batch_indices],)
+            return full_batch + (sample_weights[start:end],)
         return full_batch
 
--- pinball.py
+++ pinball.py
@@ -6,23 +6,6 @@ from tensorflow.keras.losses import Loss, Reduction
 
 TAU = tf.expand_dims(tf.cast(QUANTILES, DTYPE), 0)
 
-# input shape: (batch_size, LOSS_SIZE, HORIZON_LENGTH, Q)
-# return shape: (batch_size)
-
-AVG_FORECASTS = True
-AVG_HORIZON_LENGTH = True
-AVG_QUANTILES = True
-
-# def get_axes(avg_forecasts=False, avg_horizon_length=False, avg_quantiles=False):
-#     divide_bys = [avg_forecasts, avg_horizon_length, avg_quantiles]
-#     mean_axes = [k+1 for k in range(3) if divide_bys[k]]
-#     sum_axes = [-(k+1) for k in range(3 - sum(divide_bys))]
-#     return mean_axes, sum_axes
-
-# reduce_mean_axes, reduce_sum_axes = get_axes(AVG_FORECASTS,AVG_HORIZON_LENGTH, AVG_QUANTILES)
-# should_reduce_mean = tf.convert_to_tensor(len(reduce_mean_axes) > 0)
-# should_reduce_sum = tf.convert_to_tensor(len(reduce_sum_axes) > 0)
-
 class PinballLoss(Loss):
     def __init__(self, name='pinball_loss'):
         super().__init__(reduction=Reduction.SUM, name=name)
@@ -30,29 +13,20 @@ class PinballLoss(Loss):
     def call(self, y_true, y_pred):
         return pinball(y_true, y_pred)
 
+# input shape: (batch_size, LOSS_SIZE, HORIZON_LENGTH, Q)
+# return shape: (batch_size)
 def pinball(y_true, y_pred): # TODO apply weight 
-    batch_size = tf.shape(y_true)[0]
     def modify(y):
-        return tf.cast(y, DTYPE) # tf.reshape(tf.cast(y, DTYPE), (batch_size * LOSS_SIZE * HORIZON_LENGTH, len(QUANTILES)))
+        return tf.cast(y, DTYPE)
     y_true = modify(y_true)
     y_pred = modify(y_pred)
 
     one = tf.cast(1, DTYPE)
     delta_y = y_true - y_pred
     result = tf.math.maximum(TAU * delta_y, (TAU - one) * delta_y)
-    # result = tf.reshape(result, (batch_size, LOSS_SIZE, HORIZON_LENGTH, len(QUANTILES)))
-
-    # result = tf.cond(should_reduce_mean, lambda: tf.reduce_mean(result, axis=reduce_mean_axes), lambda: result)
-    # result = tf.cond(should_reduce_sum, lambda: tf.reduce_sum(result, axis=reduce_sum_axes), lambda: result)
-
     result = tf.reduce_mean(result, axis=(-3, -2, -1))
-
-    # result = tf.multiply(result, sample_weight) FIXME uncomment?
-    # result = tf.reduce_sum(result)
-
     # return tf.divide(result, tf.constant(12.0))
     return result
-    # return tf.multiply(result, tf.cast(batch_size, DTYPE))
 
 
 class PinballMetric(Metric):
@@ -61,82 +35,22 @@ class PinballMetric(Metric):
         self.sum = self.add_weight(name='pinball_sum', initializer='zeros', dtype=DTYPE)
         self.count = self.add_weight(name='num_samples', initializer='zeros', dtype=tf.int32)
 
-    def update_state(self, y_true, y_pred): # SAMPLE WEIGHTS DON'T WORK
-        values = pinball(y_true[:,-1], y_pred[:,-1])
+    def update_state(self, y_true, y_pred, sample_weight=None): # SAMPLE WEIGHTS DON'T WORK
+        values = pinball(y_true, y_pred)
 
-        # if sample_weight is not None: # FIXME never take this branch
-        #     sample_weight = tf.cast(sample_weight, self.dtype)
-        #     # sample_weight = tf.broadcast_weights(sample_weight, values)
-        #     values = tf.multiply(values, sample_weight)
-
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, self.dtype)
+            # sample_weight = tf.broadcast_weights(sample_weight, values)
+            values = tf.multiply(values, sample_weight)
+        else:
+            self.count.assign_add(tf.shape(values)[0])
+            
         self.sum.assign_add(tf.reduce_sum(values))
-        self.count.assign_add(tf.shape(values)[0])
 
     def result(self):
-        return tf.divide(self.sum, tf.cast(self.count, DTYPE))
+        divisor = tf.math.maximum(tf.constant(1, dtype=DTYPE), tf.cast(self.count, DTYPE))
+        return tf.divide(self.sum, divisor)
 
     def reset_states(self):
         self.sum.assign(0)
         self.count.assign(0)
-
-    
-## OLD LOGIC
-# if avg_forecasts:
-#     if avg_horizon_length:
-#         if avg_quantiles:
-#             reduce_mean_axes = (1,2,3)
-#             reduce_sum_axes = ()
-#         else:
-#             reduce_mean_axes = (1,2)
-#             reduce_sum_axes = (3,)
-#     else:
-#         if avg_quantiles:
-#             reduce_mean_axes = (1,3)
-#             reduce_sum_axes = (-1,)
-#         else:
-#             reduce_mean_axes = (1,)
-#             reduce_sum_axes = (-2,-1)
-# else:
-#     if avg_horizon_length:
-#         if avg_quantiles:
-#             reduce_mean_axes = (2,3)
-#             reduce_sum_axes = (1,)
-#         else:
-#             reduce_mean_axes = (2,)
-#             reduce_sum_axes = (-2,-1)
-#     else:
-#         if avg_quantiles:
-#             reduce_mean_axes = (3,)
-#             reduce_sum_axes = (1,2)
-#         else:
-#             reduce_mean_axes = ()
-#             reduce_sum_axes = (1,2,3)
-
-## ORIGINAL
-# @tf.function
-# def pinball_loss(
-#     y_true: TensorLike, y_pred: TensorLike, tau: FloatTensorLike = 0.5
-# ) -> tf.Tensor:
-#     """
-#     Args:
-#       y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`
-#       y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`
-#       tau: (Optional) Float in [0, 1] or a tensor taking values in [0, 1] and
-#         shape = `[d0,..., dn]`.  It defines the slope of the pinball loss. In
-#         the context of quantile regression, the value of tau determines the
-#         conditional quantile level. When tau = 0.5, this amounts to l1
-#         regression, an estimator of the conditional median (0.5 quantile).
-
-#     Returns:
-#         pinball_loss: 1-D float `Tensor` with shape [batch_size].
-#     """
-#     y_pred = tf.convert_to_tensor(y_pred)
-#     y_true = tf.cast(y_true, y_pred.dtype)
-
-#     # Broadcast the pinball slope along the batch dimension
-#     tau = tf.expand_dims(tf.cast(tau, y_pred.dtype), 0)
-#     one = tf.cast(1, tau.dtype)
-
-#     delta_y = y_true - y_pred
-#     pinball = tf.math.maximum(tau * delta_y, (tau - one) * delta_y)
-#     return tf.reduce_sum(pinball, axis=-1)
